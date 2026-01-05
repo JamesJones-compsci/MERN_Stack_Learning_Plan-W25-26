c25888458f1a134b55cc358e87460d43ed82abd5
James Jones <jjsfitnesstoronto@gmail.com>
2026-01-05 10:22:24 -0500

    feat: add user-service microservice scaffold

---
+++ backend/classmate-backend/src/main/java/com/classmate/classmatebackend/ClassmateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.classmate.classmatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassmateBackendApplication.class, args);
+	}
+
+}
---
+++ backend/classmate-backend/src/test/java/com/classmate/classmatebackend/ClassmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classmate.classmatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassmateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
