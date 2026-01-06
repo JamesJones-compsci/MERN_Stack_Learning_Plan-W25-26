b940606451f378905a0ce83c280d61f00e73e47f
James Jones <jjsfitnesstoronto@gmail.com>
2026-01-06 05:19:56 -0500

    Prepare for Docker reset: update Dockerfiles, wait-for-it, TaskServiceApplication

--- backend/classmate-backend/task-service/src/main/java/com/classmate/taskservice/TaskServiceApplication.java
+++ backend/classmate-backend/task-service/src/main/java/com/classmate/taskservice/TaskServiceApplication.java
@@ -1,5 +1,6 @@
 package com.classmate.taskservice;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TaskServiceApplication.class, args);
+    }
+
+    @PostConstruct
+    public void logMongoUri() {
+        System.out.println("Mongo URI: " + System.getProperty("spring.data.mongodb.uri"));
     }
 
 }
